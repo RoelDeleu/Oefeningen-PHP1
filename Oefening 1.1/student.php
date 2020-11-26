a3055c6deabcca43786c162e78bc665fffeb287e
RoelDeleu <roeldeleu@hotmail.com>
2020-11-26 12:31:39 +0100

    test-2

--- Oefening 1.1/student.php
+++ Oefening 1.1/student.php
@@ -1,5 +1,22 @@
 <?php
+$student =	array(
+    "voornaam" =>  "Jan",
+    "naam" =>  "Janssens",
+    "straat" =>  "Oude baan",
+    "huisnr" =>  "22",
+    "postcode" =>  2800,
+    "gemeente" =>  "Mechelen",
+    "geboortedatum" =>  "14/05/1991",
+    "telefoon" =>  "015 24 24 26",
+    "e-mail" =>  "jan.janssens@gmail.com"
+);
 
-/*
-test-2
-*/
+StudentToTable($student);
+
+function StudentToTable($student) {
+    echo "<h1>Student</h1><table>";
+    foreach($student as $key => $val) {
+        echo "<tr><td>".ucfirst($key)."</td><td>$val</td></tr>";
+    }
+    echo "</table>";
+}
